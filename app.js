d6d4b55e6f7b10e457fa74b6b2d2cd93e7e9a4a2
anujarora0502 <anujarora0502@gmail.com>
2020-05-20 03:56:38 +0530

    second

--- app.js
+++ app.js
@@ -121,6 +121,10 @@ app.get("/lessons/mathematics/pdf",(req,res)=>{
 
 });
 
-app.listen(3000,()=>{
+let port = process.env.PORT;
+if(port == null|| port == ""){
+  port = 3000;
+}
+app.listen(port,()=>{
   console.log("The server is running at port 3000");
 });
