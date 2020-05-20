3b6f3a7309d9e093a69d75e824a094aaae54f7f7
anujarora0502 <anujarora0502@gmail.com>
2020-05-20 20:17:21 +0530

    console errors removed

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,23 +1,4 @@
-let s = 0;
-let m = 1;
-var time = setInterval(function(){
-  document.querySelector(".minutes").innerHTML=m;
-  if(s<10){
-    document.querySelector(".seconds").innerHTML="0"+s;
-  }else{
-  document.querySelector(".seconds").innerHTML=s;
-  }
 
-  s--;
-  if(s==-1){
-    s=59;
-    m--;
-  }
-
-  if(m==-1){
-    document.querySelector(".quizForm").submit();
-  }
-},1000);
 
 let dt = 9;
 let ht = 0;
